1f2731c0e53329236764dbb0970f7dcc3d315a15
kianwee123 <kianwee@163.com>
2021-04-08 09:12:44 +0800

    23

--- enums.d.ts
+++ enums.d.ts
@@ -2,26 +2,45 @@
 
 
     declare const enum RemoteButton {
+    //% block=Power
     Power = 0x45,
+    //% block=Menu
     Menu = 0x47,
+    //% block=Test
     Test = 0x44,
+    //% block=Plus
     Plus = 0x40,
+    //% block=Return
     Return = 0x43,
+    //% block=Left
     Left = 0x07,
+    //% block=Play
     Play = 0x15,
+    //% block=Right
     Right = 0x09,
+    //% block=Minus
     Minus = 0x19,
+    //% block=Cancle
     Cancle = 0x0D,
-    BEEP = 0xA,
+    //% block=Num0
     NUM0 = 0x16,
+    //% block=Num1
     NUM1 = 0x0C,
+    //% block=Num2
     NUM2 = 0x18,
+    //% block=Num3
     NUM3 = 0x5E,
+    //% block=Num4
     NUM4 = 0x08,
+    //% block=Num5
     NUM5 = 0x1C,
+    //% block=Num6
     NUM6 = 0x5A,
+    //% block=Num7
     NUM7 = 0x42,
+    //% block=Num8
     NUM8 = 0x52,
+    //% block=Num9
     NUM9 = 0x4A,
     }
 
